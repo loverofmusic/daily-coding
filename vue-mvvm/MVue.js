f866f35c49611b2214fc18930e92ecabc7a5514e
loverofmusic <1073972000@qq.com>
2020-02-10 23:17:50 +0800

    精简版vue-mvvm实现

--- vue-mvvm/MVue.js
+++ vue-mvvm/MVue.js
@@ -12,11 +12,24 @@ class Compile {
     // 3. 追加子元素到根元素
     this.el.appendChild(fragment);
   }
-  compile(fragment){
+  compile(fragment) {
     // 1.获取子节点
     const childNodes = fragment.childNodes;
-    console.log([...childNodes])
-
+    // console.log([...childNodes])
+    [...childNodes].forEach(child => {
+      if (this.isElementNode(child)) {
+        console.log("元素节点", child);
+        //元素节点
+        //编译元素节点
+      } else {
+        console.log("文本节点", child);
+        //文本节点
+        //编译文本节点
+      }
+      if (child.childNodes && child.childNodes.length) {
+        this.compile(child);
+      }
+    });
   }
   isElementNode(node) {
     return node.nodeType === 1;
