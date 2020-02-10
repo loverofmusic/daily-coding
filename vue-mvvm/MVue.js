63662602f6f8c762f1c8ab132e14c6acae9968b0
loverofmusic <1073972000@qq.com>
2020-02-10 23:09:32 +0800

    精简版vue-mvvm实现

--- vue-mvvm/MVue.js
+++ vue-mvvm/MVue.js
@@ -3,9 +3,20 @@ class Compile {
     this.el = this.isElementNode(el) ? el : document.querySelector(el);
     this.vm = vm;
     // console.log(this.el);
-    // 获取文档碎片对象 放入内存中会减少页面的回流和重绘
+    // 1.获取文档碎片对象 放入内存中会减少页面的回流和重绘
     const fragment = this.node2Fragment(this.el);
-    // console.log(fragment)
+
+    // 2. 编译模版
+    this.compile(fragment);
+
+    // 3. 追加子元素到根元素
+    this.el.appendChild(fragment);
+  }
+  compile(fragment){
+    // 1.获取子节点
+    const childNodes = fragment.childNodes;
+    console.log([...childNodes])
+
   }
   isElementNode(node) {
     return node.nodeType === 1;
@@ -14,8 +25,8 @@ class Compile {
     //创建文档碎片
     const f = document.createDocumentFragment();
     let firstChild;
-    while (firstChild = el.firstChild) {
-    // while (el.firstChild) {
+    while ((firstChild = el.firstChild)) {
+      // while (el.firstChild) {
       // f.appendChild(el.firstChild);
       f.appendChild(firstChild);
     }
