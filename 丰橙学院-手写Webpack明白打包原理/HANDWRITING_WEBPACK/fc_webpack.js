aa1b95fe061cf79baa44b661441bbc094bae8f4c
loverofmusic <1073972000@qq.com>
2020-02-23 12:54:47 +0800

    面试题

--- 丰橙学院-手写Webpack明白打包原理/HANDWRITING_WEBPACK/fc_webpack.js
+++ 丰橙学院-手写Webpack明白打包原理/HANDWRITING_WEBPACK/fc_webpack.js
@@ -1,8 +1,22 @@
 const fs = require("fs");
+const parser = require("@babel/parser");
 
 function createAsset(filePath) {
+
+  //读取    入口文件内容
   const content = fs.readFileSync(filePath, "utf-8"); //sync 同步
+  console.log("------------------------------------entry file content");
   console.log(content);
+
+  //通过 @babel/parser 解析出 入口文件内容的    AST
+  const AST = parser.parse(content, {
+    sourceType: "module"
+  }); //parse 默认不支持模块化代码，所以要给定 sourceType
+  console.log("------------------------------------AST");
+  console.log(AST);
+
+  //操作AST，去分析出    依赖文件
+
 }
 
 createAsset("./src/index.js");
