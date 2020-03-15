cdc5196a861f6bbe8c68c0251398935ebebc3cbd
loverofmusic <1073972000@qq.com>
2020-03-15 20:29:23 +0800

    手写promise

---
+++ 后盾人-17.手写Promise核心代码/promise的pending状态处理/Hd.js
@@ -0,0 +1,68 @@
+class Hd {
+  static PENDING = "pending";
+  static FULFILLED = "fulfilled";
+  static REJECTED = "rejected";
+  constructor(executor){
+    this.status = Hd.PENDING;
+    this.value = null;
+    this.callbacks = [];
+    try {
+      executor(this.resolve.bind(this),this.reject.bind(this));
+    } catch (error) {
+      this.reject(error);
+    }
+  }
+  resolve(value){
+    if (this.status === Hd.PENDING) {
+      this.status = Hd.FULFILLED;
+      this.value = value;
+      this.callbacks.map(callback =>{
+        callback.onFulfilled(this.value)
+      })
+    }
+    
+  }
+  reject(err){
+    if (this.status === Hd.PENDING) {
+      this.status = Hd.REJECTED;
+      this.value = err;
+      this.callbacks.map(callback =>{
+        callback.onRejected(this.value)
+      })
+    }
+  }
+  then(onFulfilled, onRejected){
+    if(typeof onFulfilled !== "function"){
+      onFulfilled = () => {}
+    }
+    if(typeof onRejected !== "function"){
+      onRejected = () => {}
+    }
+    if(this.status === Hd.PENDING){
+      console.log('PENDING')
+      this.callbacks.push({
+        onFulfilled,
+        onRejected
+      })
+    }
+    if (this.status === Hd.FULFILLED) {
+      setTimeout(()=>{
+        try {
+          onFulfilled(this.value);
+        } catch (error) {
+          onRejected(error);
+        }
+      })
+      
+    }
+    if (this.status === Hd.REJECTED) {
+      setTimeout(()=>{
+        try {
+          onRejected(this.value);
+        } catch (error) {
+          onRejected(error);
+        }
+      })
+    }
+  }
+}
---
+++ 后盾人-17.手写Promise核心代码/then的异常捕获与异步操作/Hd.js
@@ -0,0 +1,55 @@
+class Hd {
+  static PENDING = "pending";
+  static FULFILLED = "fulfilled";
+  static REJECTED = "rejected";
+  constructor(executor){
+    this.status = Hd.PENDING;
+    this.value = null;
+    try {
+      executor(this.resolve.bind(this),this.reject.bind(this));
+    } catch (error) {
+      this.reject(error);
+    }
+    
+  }
+  resolve(value){
+    if (this.status === Hd.PENDING) {
+      this.status = Hd.FULFILLED;
+      this.value = value;
+    }
+    
+  }
+  reject(err){
+    if (this.status === Hd.PENDING) {
+      this.status = Hd.REJECTED;
+      this.value = err;
+    }
+  }
+  then(onFulfilled, onRejected){
+    if(typeof onFulfilled !== "function"){
+      onFulfilled = () => {}
+    }
+    if(typeof onRejected !== "function"){
+      onRejected = () => {}
+    }
+    if (this.status === Hd.FULFILLED) {
+      setTimeout(()=>{
+        try {
+          onFulfilled(this.value);
+        } catch (error) {
+          onRejected(error);
+        }
+      })
+      
+    }
+    if (this.status === Hd.REJECTED) {
+      setTimeout(()=>{
+        try {
+          onRejected(this.value);
+        } catch (error) {
+          onRejected(error);
+        }
+      })
+    }
+  }
+}
