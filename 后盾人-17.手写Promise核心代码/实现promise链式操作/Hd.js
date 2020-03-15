52ca18ced9ddd17b8818d45e4f35d95cbd4f2bf4
loverofmusic <1073972000@qq.com>
2020-03-15 22:41:02 +0800

    手写promise

---
+++ 后盾人-17.手写Promise核心代码/实现promise链式操作/Hd.js
@@ -0,0 +1,90 @@
+class Hd {
+  static PENDING = "pending";
+  static FULFILLED = "fulfilled";
+  static REJECTED = "rejected";
+  constructor(executor) {
+    this.status = Hd.PENDING;
+    this.value = null;
+    this.callbacks = [];
+    try {
+      executor(this.resolve.bind(this), this.reject.bind(this));
+    } catch (error) {
+      this.reject(error);
+    }
+  }
+  resolve(value) {
+    if (this.status === Hd.PENDING) {
+      this.status = Hd.FULFILLED;
+      this.value = value;
+      setTimeout(() => {
+        this.callbacks.map(callback => {
+          callback.onFulfilled(this.value);
+        });
+      });
+    }
+  }
+  reject(err) {
+    if (this.status === Hd.PENDING) {
+      this.status = Hd.REJECTED;
+      this.value = err;
+      setTimeout(() => {
+        this.callbacks.map(callback => {
+          callback.onRejected(this.value);
+        });
+      });
+    }
+  }
+  then(onFulfilled, onRejected) {
+    if (typeof onFulfilled !== "function") {
+      onFulfilled = () => {};
+    }
+    if (typeof onRejected !== "function") {
+      onRejected = () => {};
+    }
+    return new Hd((resolve, reject) => {
+      if (this.status === Hd.PENDING) {
+        console.log("PENDING");
+        this.callbacks.push({
+          // onFulfilled,
+          // onRejected
+          onFulfilled: value => {
+            try {
+              let res = onFulfilled(value);
+              resolve(res)
+            } catch (error) {
+              onRejected(error);
+            }
+          },
+          onRejected: value => {
+            try {
+              let res = onRejected(value);
+              resolve(res)
+            } catch (error) {
+              reject(error);
+            }
+          }
+        });
+      }
+      if (this.status === Hd.FULFILLED) {
+        setTimeout(() => {
+          try {
+            let res = onFulfilled(this.value);
+            resolve(res)
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
+      if (this.status === Hd.REJECTED) {
+        setTimeout(() => {
+          try {
+            let res = onRejected(this.value);
+            resolve(res)
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
+    });
+  }
+}
